f0c5de7ecd25ef744c907a2e9debcee4a3f25e39
AIPL_MPC_66\Anora <jinto@anoralabs.com>
2023-09-26 17:13:49 +0530

    hmm

--- faris/question4/question4/ret_logic_level.c
+++ faris/question4/question4/ret_logic_level.c
@@ -8,11 +8,12 @@
 #include <atmel_start.h>
 bool ret_logic_level(){
 	PIN1_J13_set_dir(PORT_DIR_IN);
-	int b=*PIN1_J13_set_dir;
+	//int b=*PIN1_J13_set_dir;
 	PIN1_J13_set_pull_mode(PORT_PULL_OFF);
-	int c=*PIN1_J13_set_pull_mode;
+	//int c=*PIN1_J13_set_pull_mode;
 	//PIN1_J13_set_level(true);
+	//PIN1_J13_set_level(false);
 	bool a= PIN1_J13_get_level();
-	int d=*PIN1_J13_get_level;
+	//int d=*PIN1_J13_get_level;
 	return a;
 }
