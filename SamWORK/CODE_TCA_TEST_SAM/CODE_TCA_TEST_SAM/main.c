cce0844be52ad98855bd0e7ee48a716536013940
ANORA\samsunny <samsunny@anorasolutions.com>
2023-10-03 12:35:47 +0530

    tca timer sam

---
+++ SamWORK/CODE_TCA_TEST_SAM/CODE_TCA_TEST_SAM/main.c
@@ -0,0 +1,16 @@
+#include "function_Call.h"
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+	/* Replace with your application code */
+	
+	
+	while (1)
+	{
+		
+		function_Call(50,100,150); // turn on LEDs at a particular brightness (r,g,b) : range=> 0-255
+		
+	}
+}
