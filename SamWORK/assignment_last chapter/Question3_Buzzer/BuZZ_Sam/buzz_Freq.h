3b560da4aa5afc6b9f223daf1d11dd37b73e622b
ANORA\samsunny <samsunny@anorasolutions.com>
2023-10-05 09:48:49 +0530

    Question 3 Sam

---
+++ SamWORK/assignment_last chapter/Question3_Buzzer/BuZZ_Sam/buzz_Freq.h
@@ -0,0 +1,18 @@
+/*
+ * buzz_Freq.h
+ *
+ * Created: 10/3/2023 2:20:30 PM
+ *  Author: samsunny
+ */ 
+
+
+#ifndef BUZZ_FREQ_H_
+#define BUZZ_FREQ_H_
+
+#include "tcb.h"
+#include <atmel_start.h>
+#include <stdint.h>
+
+void buzz_Freq(uint16_t freq);
+
+#endif /* BUZZ_FREQ_H_ */
