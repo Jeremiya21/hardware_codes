77439ef9e8ff8244f5452b6481ed8ed3429f17d0
AIPL_MPC_66\Anora <jinto@anoralabs.com>
2023-10-07 13:09:19 +0530

    changed

--- Jeremiya/Timer assign/Timer2/Timer2/led_cntrl.c
+++ Jeremiya/Timer assign/Timer2/Timer2/led_cntrl.c
@@ -8,10 +8,10 @@
 #include "led_cntrl.h"
 void led_cntrl(uint8_t red,uint8_t green, uint8_t blue)
 {
-	TCA0_SINGLE_CMP0BUF=red;
+	TCA0.SINGLE.CMP0BUF=red;
 	//TCA0_SINGLE_CMP0=red;
-	TCA0_SINGLE_CMP1BUF=green;
+	TCA0.SINGLE.CMP1BUF=green;
 	//TCA0_SINGLE_CMP1=green;
-	TCA0_SINGLE_CMP2BUF=blue;
+	TCA0.SINGLE.CMP2BUF=blue;
 	//TCA0_SINGLE_CMP2=blue;
 }
